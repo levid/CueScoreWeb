431f4c0130352d5e9ff6fb6e1a1b79ef55f42b88
Isaac Wooten <i.wooten@gmail.com>
2012-08-09 12:07:22 -0700

    first commit

---
+++ app/models/post.rb
@@ -0,0 +1,3 @@
+class Post < ActiveRecord::Base
+  attr_accessible :address, :desire, :dob, :hobby, :interests, :name, :signature
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guestbook::Application.config.secret_token = '62b1150cb6d01c292cda40ae0a63bec9cdb79747189f5c443d6068166718e9176aa1b5681f08be6f20a98b2b35c0ba757171a2f123951a8f14eb206ebed1731f'
